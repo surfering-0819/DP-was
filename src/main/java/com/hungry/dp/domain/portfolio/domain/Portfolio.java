85a07a54cfbd6dcdb5c0ba2f4dec70d03122b1cc
tentenball <lth8905@gmail.com>
2024-08-20 00:57:40 +0900

    feat: 엔티티 작성

---
+++ src/main/java/com/hungry/dp/domain/portfolio/domain/Portfolio.java
@@ -0,0 +1,30 @@
+package com.hungry.dp.domain.portfolio.domain;
+
+import com.hungry.dp.common.domain.BaseEntity;
+import com.hungry.dp.domain.activity.domain.Activity;
+import com.hungry.dp.domain.user.domain.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Portfolio extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Activity> activities = new ArrayList<>();
+}
--- src/main/java/com/hungry/dp/domain/user/domain/User.java
+++ src/main/java/com/hungry/dp/domain/user/domain/User.java
@@ -1,11 +1,15 @@
 package com.hungry.dp.domain.user.domain;
 
+import com.hungry.dp.domain.portfolio.domain.Portfolio;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -34,7 +38,11 @@ public class User {
     @NotNull
     private Grade grade;
 
-    private int weight;
+    private int weight = 0;
+
+    @OneToMany(mappedBy = "user")
+    private List<Portfolio> portfolios = new ArrayList<>();
+
     @Builder
     protected User(String identify, String password, String school, String job){
         this.identify = identify;
